61ff037f23021313b9233fb52bb1f7104a72c4d9
LinasRam <linas.ramanauskas95@gmail.com>
2016-04-06 18:44:45 +0300

    Visas žaidimas

---
+++ snake.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gyvatėlė</title>
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="js/snake.js"></script>
+
+</body>
+</html>
